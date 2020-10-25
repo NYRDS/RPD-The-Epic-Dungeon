f6224174810de09d779e74418f1580a815a003e2
Mikhael  Danilov <mikhael.danilov@gmail.com>
2020-10-25 23:09:27 +0300

    EPD.Char would be nil

--- The epic dungeon/scripts/buffs/Attacher.lua
+++ The epic dungeon/scripts/buffs/Attacher.lua
@@ -6,12 +6,12 @@ local EPD  = require "scripts/lib/dopClasses"
 
 local buff = require "scripts/lib/buff"
 
-return buff.init{
-    desc  = function ()
+return buff.init {
+    desc     = function()
         return {
-            icon          = -1,
+            icon = -1,
             name = "CounterBuff_Nabuff.target",
-            info          = "CounterBuff_Info",
+            info = "CounterBuff_Info",
         }
     end,
 
@@ -19,34 +19,36 @@ return buff.init{
         return true
     end,
 
-    charAct = function(self,buff)
-f = EPD.Char
-buff.target:beckon(f:getPos())
---EPD.Char = nil
-for i = 1, RPD.Dungeon.level:getLength()-1 do
-local maybeMob = RPD.Actor:findChar(i)          
-if maybeMob and maybeMob ~= RPD.Dungeon.hero and not maybeMob:isPet() then
-if buff.target:canAttack(maybeMob) then
-if RPD.Dungeon.level:distance(buff.target:getPos(),i) == 1 then
---maybeMob:damage(buff.target:damageRoll(),buff.target)
-buff.target:getSprite():attack(maybeMob:getPos())
-buff.target:spend(buff.target:speed())
-buff.target:attack(maybeMob)
-break
-else
---maybeMob:damage(buff.target:damageRoll(),buff.target)
-buff.target:getSprite():zap(maybeMob:getPos())
-buff.target:spend(buff.target:speed())
-buff.target:attack(maybeMob)
-buff.target:zap(maybeMob)
-break
-end
-end
-end
-end
---RPD.glog(tostring(buff.target:defenceRoll(buff.target)))
---RPD.glog(tostring(buff.target:attackSkill()))
+    charAct  = function(self, buff)
+        f = EPD.Char
 
-buff.target:spend(1)
-end
+        if f then
+            buff.target:beckon(f:getPos())
+            --EPD.Char = nil
+            for i = 1, RPD.Dungeon.level:getLength() - 1 do
+                local maybeMob = RPD.Actor:findChar(i)
+                if maybeMob and maybeMob ~= RPD.Dungeon.hero and not maybeMob:isPet() then
+                    if buff.target:canAttack(maybeMob) then
+                        if RPD.Dungeon.level:distance(buff.target:getPos(), i) == 1 then
+                            --maybeMob:damage(buff.target:damageRoll(),buff.target)
+                            buff.target:getSprite():attack(maybeMob:getPos())
+                            buff.target:spend(buff.target:speed())
+                            buff.target:attack(maybeMob)
+                            break
+                        else
+                            --maybeMob:damage(buff.target:damageRoll(),buff.target)
+                            buff.target:getSprite():zap(maybeMob:getPos())
+                            buff.target:spend(buff.target:speed())
+                            buff.target:attack(maybeMob)
+                            buff.target:zap(maybeMob)
+                            break
+                        end
+                    end
+                end
+            end
+            --RPD.glog(tostring(buff.target:defenceRoll(buff.target)))
+            --RPD.glog(tostring(buff.target:attackSkill()))
+        end
+        buff.target:spend(1)
+    end
 }
